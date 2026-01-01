02bd0748d6f4729c451f61f5f5c94d94c8e1ab93
Sharaf Ahmed <245864029+Sharaf-Ahmed-Dev@users.noreply.github.com>
2026-01-01 20:32:14 +0530

    Initial commit: Add daily task manager web application

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+
+    if (inputBox.value === '') {
+        alert("Please enter a task!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    inputBox.value = '';
+    storagedata();
+}
+
+
+listContainer.addEventListener("click", event => {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+        storagedata();
+    }
+    else if (event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        storagedata();
+
+    }
+}, false);
+
+
+
+function storagedata() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+
+function showdata() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showdata();
